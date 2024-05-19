17e164a288ddda60e5d49c2288bd1d9423abf539
Muhammad Raffey <muhammadraffey26@gmail.com>
2024-05-19 18:39:27 +0500

    small update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -12,13 +12,6 @@ export default function NavBar() {
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-      </NavbarContent>
-
       <NavbarContent className=" sm:flex gap-4 justify-center mr-10">
         <NavbarItem>
           <Link color="foreground" href="/">
